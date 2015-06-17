f7fcd1e5eaaf2b7a315f5dcdfac808e53e60c4cd
Roan Huang <pichuang@cs.nctu.edu.tw>
2015-06-17 10:47:50 +0800

    add more docker-compose file

--- icinga2/README.md
+++
@@ -1,6 +0,0 @@
-icinga2 on Docker
-=================
-
-```http://<servername>:8001/icinga-web```
-
-
--- smokeping/run_by_command_line.sh
+++ smokeping/run_by_command_line.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # http://night9.cc:8000/smokeping/smokeping.cgi
-docker run --name smokeping -p 8000:80 -v ~/docker/smokeping:/etc/smokeping:ro\
-            -d dperson/smokeping -T Asia/Taipei
+docker run --name smokeping -p 8000:80 -v ~/docker/smokeping:/etc/smokeping:ro -d dperson/smokeping -T Asia/Taipei
